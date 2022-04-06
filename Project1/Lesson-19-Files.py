fc1d019a2b7cd07c3f5e8c470c5c06b0014d931b
Slava Danilau <v.danilau@yandex.by>
2022-04-06 17:28:47 +0500

    Change hard code to variable.

--- Project1/Lesson-19-Files.py
+++ Project1/Lesson-19-Files.py
@@ -7,9 +7,9 @@ myfile1 = open(inputfile, mode='r', encoding='latin_1')
 myfile2 = open(outfile, mode='w', encoding='latin_1')
 
 for num, line in enumerate(myfile1, 1):  # нумерует с 1, при этом в line заходит все еще каждая строчка из файла, а вот в num уже идет номерное значение.
-    if "Davidson" in line:
+    if password_to_look_to in line:
         print("Line №:  " + str(num) + " : " + line.strip())
         myfile2.write("Found password: " + line)
 
 myfile1.close()
-myfile2.close()
+myfile2.close() 
